d1a6131d818fbbc7dded3e43a90724eca16507e4
ZumeShougo <g0109725rena@gmail.com>
2022-05-01 15:17:10 +0900

    fixed TimeZone

--- config/application.rb
+++ config/application.rb
@@ -39,5 +39,7 @@ module RunteqNormal
     config.i18n.available_locales = %i[ja en]
     config.i18n.default_locale = :ja #デフォルトの言語を日本語にする
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s] #i18nの複数のロケールファイルが読み込まれるようにpathを通す
+    config.time_zone = 'Tokyo'	
+    config.active_record.default_timezone = :local
   end
 end
