3f17c7578fbc4e24e8ebbfbe9faea7d1fbaaa11d
ZumeShougo <g0109725rena@gmail.com>
2022-05-12 21:13:34 +0900

    add board_image

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -22,8 +22,7 @@ class BoardsController < ApplicationController
   end
 
   private
-
   def board_params
-    params.require(:board).permit(:title, :body)
+    params.require(:board).permit(:title, :body, :board_image, :board_image_cache)
   end
 end
