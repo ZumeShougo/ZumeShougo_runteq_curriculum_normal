89c9578b299f91dc087186ac4824c4a44310bdbd
ZumeShougo <g0109725rena@gmail.com>
2022-05-14 13:17:07 +0900

    add comment_controller

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,13 @@
+class CommentsController < ApplicationController
+  def create
+    comment = current_user.comments.build(comment_params)
+    if comment.save?
+      redirect_to board_path(comment.board), success: t('')
+    else
+      redirect_to board_path(comment.board), danger: t('')
+    end
+  end
+  private
+  def comment_params
+    params.require(:comment).permit(:body).merge(board_id: params[:board_id])
+end
