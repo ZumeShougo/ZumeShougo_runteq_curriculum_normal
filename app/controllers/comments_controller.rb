2431f77e17f8ab40ce0958e4fc145d8ef8ab8be3
ZumeShougo <g0109725rena@gmail.com>
2022-05-16 20:36:49 +0900

    add comments_controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,13 +1,14 @@
 class CommentsController < ApplicationController
   def create
     comment = current_user.comments.build(comment_params)
-    if comment.save?
-      redirect_to board_path(comment.board), success: t('')
+    if comment.save
+      redirect_to board_path(comment.board), success: t('defaults.message.created')
     else
-      redirect_to board_path(comment.board), danger: t('')
+      redirect_to board_path(comment.board), danger: t('defaults.message.not_created')
     end
   end
   private
   def comment_params
     params.require(:comment).permit(:body).merge(board_id: params[:board_id])
+  end
 end
