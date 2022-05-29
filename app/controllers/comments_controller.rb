658b1854f47f9a727b694aa0ccfeda189bfae4c2
ZumeShougo <g0109725rena@gmail.com>
2022-05-29 16:17:07 +0900

    ajax

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,11 +1,12 @@
 class CommentsController < ApplicationController
   def create
-    comment = current_user.comments.build(comment_params)
-    if comment.save
-      redirect_to board_path(comment.board), success: t('defaults.message.created')
-    else
-      redirect_to board_path(comment.board), danger: t('defaults.message.not_created')
-    end
+    @comment = current_user.comments.build(comment_params)
+    @comment.save
+  end
+
+  def destroy
+    @comment = current_user.comments.find(params[:id])
+    @comment.destroy!
   end
   private
   def comment_params
