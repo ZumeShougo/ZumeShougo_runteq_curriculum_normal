adcb3cc8225050b9fc02bcf54f0ac95acca124c4
ZumeShougo <g0109725rena@gmail.com>
2022-06-07 04:54:36 +0900

    add reset_password_modules

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,6 +14,7 @@ class User < ApplicationRecord
   validates :email, presence: true
   validates :first_name, presence: true, length: { maximum: 255 }
   validates:last_name, presence: true, length: {maximum: 255 }
+  validates :reset_password_token, presence: true, uniqueness: true
 
   def own?(object)
     id == object.user_id
