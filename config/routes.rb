556e7cac0477d08e3eaa099fbf527f7a23acb190
ZumeShougo <g0109725rena@gmail.com>
2022-07-06 20:02:57 +0900

    fix initializers,locales

--- config/routes.rb
+++ config/routes.rb
@@ -16,4 +16,11 @@ Rails.application.routes.draw do
   end
   resources :bookmarks, only: %i[create destroy]
   resources :password_resets, only: %i[new create edit update]
+
+  namespace :admin do
+    root to: 'dashboards#index'
+    get 'login', to: 'user_sessions#new'
+    post 'login', to: 'user_sessions#create'
+    delete 'logout', to: 'user_sessions#destroy'
+  end
 end
