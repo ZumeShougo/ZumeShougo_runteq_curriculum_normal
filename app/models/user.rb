ee90559d2be94186b0a76aaac972fba13cbe286e
ZumeShougo <g0109725rena@gmail.com>
2022-07-06 20:00:53 +0900

    fix User

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,7 +15,8 @@ class User < ApplicationRecord
   validates :email, presence: true
   validates :first_name, presence: true, length: { maximum: 255 }
   validates:last_name, presence: true, length: {maximum: 255 }
-  validates :reset_password_token, presence: true, uniqueness: true
+
+  enum role: { general: 0, admin: 1 }
 
   def own?(object)
     id == object.user_id
