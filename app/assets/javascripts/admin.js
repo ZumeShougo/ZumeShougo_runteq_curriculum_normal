a96f249395c31d080e1edd85540ec3ba525369ff
ZumeShougo <g0109725rena@gmail.com>
2022-07-06 19:58:01 +0900

    add images, fix javascript stylesheet

--- app/assets/javascripts/admin.js
+++ app/assets/javascripts/admin.js
@@ -1,4 +1,4 @@
 //= require jquery3
 //= require rails-ujs
-//= require admin-lte/plugin/bootstrap/js/bootstrap.bundle.min
-//= require admin-lte/dist/js/adminlte.js
+//= require admin-lte/plugins/bootstrap/js/bootstrap.bundle.min
+//= require admin-lte/dist/js/adminlte
