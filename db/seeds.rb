4fe6e1194ce1368621f7fa699f37e1839ad5f8e6
ZumeShougo <g0109725rena@gmail.com>
2022-07-09 08:06:35 +0900

    fix pagenete, add locale, fix seeds, add enum_help

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-10.times do
+30.times do
   User.create(
       first_name: Faker::Name.first_name,
       last_name: Faker::Name.last_name,
@@ -22,8 +22,3 @@ end
       body: "本文#{index}"
   )
 end
-
-Admin.create(
-      email: 'admin@example.com',
-      password: 'password',
-)
