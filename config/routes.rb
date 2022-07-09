6740060c4146665144f453f1bc5336da65ec78fa
ZumeShougo <g0109725rena@gmail.com>
2022-07-09 13:23:10 +0900

    fix initializers,routes

--- config/routes.rb
+++ config/routes.rb
@@ -23,5 +23,6 @@ Rails.application.routes.draw do
     post 'login', to: 'user_sessions#create'
     delete 'logout', to: 'user_sessions#destroy'
     resources :users, only: [:index, :show, :edit, :update, :destroy]
+    resources :boards, only: [:index, :show, :edit, :update, :destroy]
   end
 end
